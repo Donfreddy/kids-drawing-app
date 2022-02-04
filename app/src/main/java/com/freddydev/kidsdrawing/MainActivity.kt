3d084c32630c018697d813651c32c65ef7601f8a
Don Freddy <freddytamwo@gmail.com>
2022-02-04 15:34:04 +0100

    first commit

---
+++ app/src/main/java/com/freddydev/kidsdrawing/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.freddydev.kidsdrawing
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * https://ssaurel.medium.com/learn-to-create-a-paint-application-for-android-5b16968063f8
+ */
+class MainActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
