5eb04490bc7deae001e498e1aae5c0c276039b90
Don Freddy <freddytamwo@gmail.com>
2022-02-07 13:07:49 +0100

    add change bg and get it from external storage

--- app/src/main/java/com/freddydev/kidsdrawing/MainActivity.kt
+++ app/src/main/java/com/freddydev/kidsdrawing/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.freddydev.kidsdrawing
 
+import android.Manifest
+import android.app.Activity
+import android.app.AlertDialog
 import android.app.Dialog
+import android.content.Intent
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
@@ -8,10 +12,11 @@ import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
-import android.widget.ImageButton
-import android.widget.LinearLayout
-import android.widget.Toast
+import android.widget.*
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
 import yuku.ambilwarna.AmbilWarnaDialog
@@ -21,15 +26,37 @@ import yuku.ambilwarna.AmbilWarnaDialog.OnAmbilWarnaListener
 /**
  * https://ssaurel.medium.com/learn-to-create-a-paint-application-for-android-5b16968063f8
  */
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
 
   private lateinit var drawingView: DrawingView
   private lateinit var brushBtn: ImageButton
+  private lateinit var galleryBtn: ImageButton
   private lateinit var smallBtn: ImageButton
   private lateinit var mediumBtn: ImageButton
   private lateinit var llPaintColors: LinearLayout
   private lateinit var largeBtn: ImageButton
+  private lateinit var bgImage: ImageView
   private var mImageButtonCurrentPaint: ImageButton? = null
+
+  private val requestPermission: ActivityResultLauncher<Array<String>> =
+    registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+      permissions.entries.forEach {
+        val permissionName = it.key
+        val isGranted = it.value
+
+        if (isGranted) {
+          chooseImageGallery()
+        } else {
+          if (permissionName == Manifest.permission.READ_EXTERNAL_STORAGE) {
+            showRationaleDialog(
+              "Kids Drawing",
+              "Kids Drawing App needs to Access Your External Storage"
+            )
+          }
+        }
+      }
+    }
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -38,8 +65,10 @@ class MainActivity : AppCompatActivity() {
     supportActionBar?.setBackgroundDrawable(ColorDrawable(Color.parseColor("#005F73")))
 
     drawingView = findViewById(R.id.drawing_view)
-    brushBtn = findViewById(R.id.brush)
+    brushBtn = findViewById(R.id.ib_brush)
+    galleryBtn = findViewById(R.id.ib_gallery)
     llPaintColors = findViewById(R.id.ll_paint_colors)
+    bgImage = findViewById(R.id.iv_bg)
     mImageButtonCurrentPaint = llPaintColors[1] as ImageButton
 
     // set Image drawable to pallet pressed
@@ -55,6 +84,12 @@ class MainActivity : AppCompatActivity() {
       showBrushDialog()
 
       // openColorPickerDialogue();
+    }
+
+    galleryBtn.setOnClickListener {
+
+      requestStoragePermission()
+
     }
   }
 
@@ -128,6 +163,19 @@ class MainActivity : AppCompatActivity() {
     }
   }
 
+  private fun chooseImageGallery() {
+    val intent = Intent(Intent.ACTION_PICK)
+    intent.type = "image/*"
+    startActivityForResult(intent, IMAGE_CHOOSE)
+  }
+
+  override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+    super.onActivityResult(requestCode, resultCode, data)
+    if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_CHOOSE) {
+      bgImage.setImageURI(data?.data)
+    }
+  }
+
   // https://www.geeksforgeeks.org/how-to-create-a-basic-color-picker-tool-in-android
   private fun openColorPickerDialogue() {
     var defaultColor = Color.parseColor(mImageButtonCurrentPaint?.tag.toString())
@@ -140,5 +188,45 @@ class MainActivity : AppCompatActivity() {
         }
       })
     colorPickerDialogue.show()
+  }
+
+  private fun requestStoragePermission() {
+    if (ActivityCompat.shouldShowRequestPermissionRationale(
+        this,
+        Manifest.permission.READ_EXTERNAL_STORAGE
+      )
+    ) {
+      showRationaleDialog("Kids Drawing", "Kids Drawing App needs to Access Your External Storage")
+    } else {
+      requestPermission.launch(arrayOf((Manifest.permission.READ_EXTERNAL_STORAGE)))
+      // Todo: Add writing external permission
+    }
+  }
+
+  /**
+   * Show rational dialog for displaying why the app needs .
+   * Only shown if the user has denied the permission request previously
+   */
+  private fun showRationaleDialog(title: String, message: String) {
+    val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+    builder.setTitle(title).setMessage(message)
+      .setPositiveButton("Cancel") { dialog, _ -> dialog.dismiss() }
+    builder.create().show()
+  }
+
+  /**
+   * Method is used to show the Custom Progress Dialog
+   */
+  private fun customProgressDialog() {
+    val customProgressDialog = Dialog(this)
+
+    customProgressDialog.setContentView(R.layout.dialog_custom_progress)
+
+    customProgressDialog.show()
+  }
+
+  companion object {
+    private const val IMAGE_CHOOSE = 1000;
+    private const val PERMISSION_CODE = 1001;
   }
 }
